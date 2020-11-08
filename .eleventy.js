f504688c7c40eb0ae5dcb81f785535da4335fbc8
Ruby Quail <ruby@rubyquail.design>
2020-11-08 18:55:37 +1100

    starting to do good data stuff
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,13 @@
 const pluginSass = require("eleventy-plugin-sass");
 const fs = require('fs');
 const path = require('path')
+const yaml = require('js-yaml');
 const cacheBuster = require("@mightyplow/eleventy-plugin-cache-buster");
 const { exec } = require("child_process");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.setTemplateFormats(['pug', 'scss', 'svg'])
+  eleventyConfig.addDataExtension('yaml', c => yaml.safeLoad(c))
   // eleventyConfig.addPassthroughCopy('scripts/*.js')
   eleventyConfig.addPlugin(pluginSass);
   eleventyConfig.addWatchTarget('./src/')
--- src/pier.ts
+++ src/pier.ts
@@ -2,7 +2,7 @@ import noise from './simplex-noise.js';
 let ns = new noise();
 const TAU = Math.PI * 2;
 const CYCLE_FRAMES = 8 * 60;
-const RESOLUTION = 64;
+const RESOLUTION = Math.floor(window.innerWidth / 8);
 const RADIUS = 0.5;
 const WID_MULT = 0.1;
 const sea = document.querySelector('#pr-sea');
@@ -12,10 +12,11 @@ function draw() {
   const periodAngle = ((frameCount % CYCLE_FRAMES) / CYCLE_FRAMES) * TAU;
 
   for (let x = 0; x <= RESOLUTION; x++) {
+    const xOff = x / RESOLUTION * 64;
     const y = 85 + 3*Math.sin(ns.noise3D(
-      Math.cos(periodAngle-x/8) * RADIUS,
-      Math.sin(periodAngle-x/8) * RADIUS,
-      x * WID_MULT
+      Math.cos(periodAngle-xOff/8) * RADIUS,
+      Math.sin(periodAngle-xOff/8) * RADIUS,
+      xOff * WID_MULT
     ));
     pts.push(`${((x/RESOLUTION)*100).toFixed(3)} ${y.toFixed(3)}`)
   }
