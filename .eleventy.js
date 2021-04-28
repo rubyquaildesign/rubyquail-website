cdc0fa50256e93e5e24f8955c4e8ef594ac2807e
Ruby Quail <ruby@rubyquail.design>
2021-04-28 16:24:32 +1000

    well, I started on cosmos
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- .eleventy.js
+++ .eleventy.js
@@ -1,31 +1,28 @@
 //@ts-check
-const pluginSass = require("eleventy-plugin-sass");
+const pluginSass = require('eleventy-plugin-sass');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const yaml = require('js-yaml');
-const cacheBuster = require("@mightyplow/eleventy-plugin-cache-buster");
-const { exec } = require("child_process");
+const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
+const { exec } = require('child_process');
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.setTemplateFormats(['pug', 'scss'])
-  eleventyConfig.addPassthroughCopy('img')
-  eleventyConfig.addPassthroughCopy('icns')
-  eleventyConfig.addPassthroughCopy('./*.png')
-  eleventyConfig.addPassthroughCopy('scripts')
-  eleventyConfig.addDataExtension('yaml', c => yaml.safeLoad(c))
+  eleventyConfig.setTemplateFormats(['pug', 'scss']);
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('icns');
+  eleventyConfig.addPassthroughCopy('./*.png');
+  eleventyConfig.addPassthroughCopy('scripts');
+  eleventyConfig.addDataExtension('yaml', (c) => yaml.safeLoad(c));
   // eleventyConfig.addPassthroughCopy('scripts/*.js')
   eleventyConfig.addPlugin(pluginSass);
-  eleventyConfig.addWatchTarget('./src/')
-  eleventyConfig.addWatchTarget('./*.md')
+  eleventyConfig.addWatchTarget('./src/');
+  eleventyConfig.addWatchTarget('./**/*.md');
   eleventyConfig.on('beforeBuild', (changed) => {
     // return new Promise((res) => {
     //   exec('tsc', () => {
     //     res();
     //   });
-
     // })
-  })
-  return {
-
-  }
-}
+  });
+  return {};
+};
---
+++ projects/cosm-os/content.md
@@ -0,0 +1,9 @@
+# Cosm.os
+
+## Intro
+
+Cosm.os was my industrial design honours project.
+it was born out of a mix of my fascination with
+systems thinking, and my difficulty reconciling
+the positive community aspects of social media and
+the negative aspects
