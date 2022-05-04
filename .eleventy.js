601be65a9e818011e9e51161186d624d69b6e346
Ruby Quail <ruby@rubyquail.design>
2022-05-04 15:27:30 +1000

    hellsite
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,29 @@
 //@ts-check
-const pluginSass = require('eleventy-plugin-sass')
 const fs = require('fs')
 const path = require('path')
 /** @type any */
 const yaml = require('js-yaml')
-const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster')
-const { exec } = require('child_process')
 
+
+const sass = require('sass')
 module.exports = (eleventyConfig) => {
-  eleventyConfig.setTemplateFormats(['pug', 'scss'])
-  eleventyConfig.addPassthroughCopy('img')
-  eleventyConfig.addPassthroughCopy('icns')
-  eleventyConfig.addPassthroughCopy('./*.png')
-  eleventyConfig.addPassthroughCopy('scripts')
-  eleventyConfig.addDataExtension('yaml', (c) => yaml.safeLoad(c))
-  // eleventyConfig.addPassthroughCopy('scripts/*.js')
-  eleventyConfig.addPlugin(pluginSass)
-  eleventyConfig.addWatchTarget('./src/')
-  eleventyConfig.addWatchTarget('./**/*.md')
-  eleventyConfig.on('beforeBuild', (changed) => {
-    // return new Promise((res) => {
-    //   exec('tsc', () => {
-    //     res();
-    //   });
-    // })
+  eleventyConfig.addTemplateFormats('scss')
+  eleventyConfig.addExtension('scss', {
+    outputFileExtension: 'css',
+    compile: async function (inputContent, inputPath) {
+
+      const parsed = path.parse(inputPath)
+      let result = sass.compileString(inputContent, {
+        loadPaths:[parsed.dir ?? '.',this.config.dir.includes]
+      });
+      return async (data) => result.css
+    }
   })
-  return {}
+  eleventyConfig.addPassthroughCopy('src/icons')
+  eleventyConfig.addPassthroughCopy('../dist/*')
+  eleventyConfig.addDataExtension('yaml', (c) => yaml.safeLoad(c))
+  return {
+    dir: { input: 'src', output: '_site' },
+    templateFormats: ['html', 'pug', 'scss']
+  }
 }
