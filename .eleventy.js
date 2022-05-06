025a143fdb8a9c5f6425cb27640bb5f38bf66684
Ruby Quail <ruby@rubyquail.design>
2022-05-06 16:03:56 +1000

    eyy
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
-//@ts-check
 const fs = require('fs')
 const path = require('path')
 /** @type any */
 const yaml = require('js-yaml')
 const sv = require('./shortcodes/svelteShort.cjs')
-
+const rollup = require('rollup');
+const svelte = require('rollup-plugin-svelte');
+const ts = require('@rollup/plugin-typescript');
+const nodeResolve = require('@rollup/plugin-node-resolve');
 const sass = require('sass')
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
   eleventyConfig.addTemplateFormats('scss')
   eleventyConfig.addAsyncShortcode('svelte',sv)
@@ -20,13 +23,36 @@ module.exports = (eleventyConfig) => {
       return async (data) => result.css
     }
   })
+  eleventyConfig.addExtension('ts', {
+    outputFileExtension:'js',
+    compile: async function (content, inputPath) {
+      const build = await rollup.rollup({
+        input: inputPath,
+        external:/\.\//,
+        plugins: [ts(),
+          ,nodeResolve.default({
+            browser: true,
+            dedupe: ['svelte'],
+          }),]
+          
+      });
+  
+      const { output: [ main ] } = await build.generate({
+        format: 'esm',
+      });
+    
+      if (main.facadeModuleId) {
+        return  async (data) => main.code;
+      }
+    }
+  })
   eleventyConfig.addPassthroughCopy('src/icons')
   eleventyConfig.addPassthroughCopy('../dist/*')
   eleventyConfig.addWatchTarget('*.md')
   eleventyConfig.addDataExtension('yaml', (c) => yaml.safeLoad(c))
   return {
     dir: { input: 'src', output: '_site' },
-    templateFormats: ['html', 'pug', 'scss', 'md','11ty.js'],
+    templateFormats: ['html', 'pug', 'scss', 'md','11ty.js','ts'],
     markdownTemplateEngine: "njk"
   }
 }
