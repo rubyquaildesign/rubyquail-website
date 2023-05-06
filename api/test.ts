bbcaf8841acec660a966f2931951413aa6ddce3a
Ruby Quail <ruby@rubyquail.design>
2023-05-06 14:26:42 +1000

    someTesting
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- api/test.ts
+++ api/test.ts
@@ -2,8 +2,13 @@ import fs from 'node:fs/promises';
 import process from 'node:process';
 import path from 'node:path';
 
+async function recursePath(currentPath: string, depth = 0) {
+	if fs.d
+};
+
 export default async function handler(request, response) {
 	const data = await fs.readdir(process.cwd());
+	fullPaths = data.map(file => path.join(process.cwd(), file));
 
 	response.status(200).json({
 		body: data.join(' , '),
