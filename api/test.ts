4457925a16471d5d51d243a83b7d285fe5dd1df0
Ruby Quail <ruby@rubyquail.design>
2023-05-06 14:15:35 +1000

    testing accessible files
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- api/test.ts
+++ api/test.ts
@@ -1,6 +1,12 @@
-export default function handler(request, response) {
+import fs from 'node:fs/promises';
+import process from 'node:process';
+import path from 'node:path';
+
+export default async function handler(request, response) {
+	const data = await fs.readdir(process.cwd());
+
 	response.status(200).json({
-		body: `Hey`,
+		body: data.join(' , '),
 		query: request.query,
 		cookies: request.cookies,
 	});
