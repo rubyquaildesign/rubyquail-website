8d37c7726dae27623e8c708da400624539779c6c
Ruby Quail <ruby@rubyquail.design>
2024-05-12 17:38:17 +1000

    some updates
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- api/_signature.ts
+++ api/_signature.ts
@@ -1,1 +1,43 @@
-export
+import { Sha256Signer } from 'activitypub-http-signatures';
+const domain =
+  process.env.VERCEL_ENV === 'development'
+    ? 'ruby-dev.tunnelto.dev'
+    : process.env.VERCEL_URL ?? '';
+export async function signAndSend(
+  url: string,
+  headers: Record<string, unknown>,
+  method: 'GET' | 'POST',
+  body?: string,
+  key: string = `https://${domain}/api/pub/blog/actor#main-key`,
+): Promise<Response> {
+  const keyID = `https://${domain}/api/pub/blog/actor#main-key`;
+
+  console.log(process.env.APUB_PRIVATE);
+  const targetUrl = new URL(url);
+  const signHeaders = {
+    host: targetUrl.host,
+    date: new Date().toUTCString(),
+  };
+  const signer = new Sha256Signer({
+    privateKey: process.env.APUB_PRIVATE ?? '',
+    publicKeyId: keyID,
+    headerNames: [...Object.keys(signHeaders), '(request-target)'],
+  });
+  console.log(signer);
+  const signature = signer.sign({
+    url: targetUrl.toString(),
+    headers: signHeaders,
+    method: 'GET',
+  });
+  console.log(signature);
+  const data = await fetch(url, {
+    body,
+    method,
+    headers: {
+      ...headers,
+      ...signHeaders,
+      signature,
+    },
+  });
+  return data;
+}
--- api/pub/blog/_actor_get.ts
+++ api/pub/blog/_actor_get.ts
@@ -2,29 +2,33 @@ import * as process from 'node:process';
 import { type VercelResponse } from '@vercel/node';
 import { type APActor } from 'activitypub-types';
 
-const domain = process.env.VERCEL_URL;
-export default function get(resp: VercelResponse) {
-	resp.statusCode = 200;
-	const actor = {
-		'@context': ['https://www.w3.org/ns/activitystreams'],
-		type: 'Person',
-		id: `https://${domain}/api/blog/actor`,
-		outbox: `https://${domain}/api/blog/outbox`,
-		inbox: `https://${domain}/api/blog/inbox`,
-		preferredUsername: 'blog',
-		name: "Ruby Quail's Blog",
-		summary: '',
-		publicKey: {
-			'@context': 'https://w3id.org/security/v1',
-			'@type': 'Key',
-			id: `https://${domain}/api/blog/actor#main-key`,
-			owner: `https://${domain}/api/blog/actor`,
-			publicKeyPem: process.env.APUB_Public,
-		},
-	} satisfies APActor & { publicKey: any };
-	resp
-		.setHeader('Content-Type', 'application/activity+json')
-		.status(200)
-		.json(actor)
-		.end();
+const domain =
+  process.env.VERCEL_ENV === 'development'
+    ? 'ruby-dev.tunnelto.dev'
+    : process.env.VERCEL_URL ?? '';
+export default function get(resp: VercelResponse): void {
+  resp.statusCode = 200;
+  const actor = {
+    '@context': ['https://www.w3.org/ns/activitystreams'],
+    type: 'Person',
+    id: `https://${domain}/api/pub/blog/actor`,
+    outbox: `https://${domain}/api/pub/blog/outbox`,
+    inbox: `https://${domain}/api/pub/blog/inbox`,
+    preferredUsername: 'blog',
+    name: "Ruby Quail's Blog",
+    summary: '',
+    publicKey: {
+      '@context': 'https://w3id.org/security/v1',
+      '@type': 'Key',
+      id: `https://${domain}/api/pub/blog/actor#main-key`,
+      owner: `https://${domain}/api/pub/blog/actor`,
+      publicKeyPem: process.env.APUB_PUBLIC,
+    },
+  } satisfies APActor & { publicKey: any };
+  resp
+    .setHeader('Content-Type', 'application/activity+json')
+    .status(200)
+    .json(actor)
+    .end();
+  return;
 }
